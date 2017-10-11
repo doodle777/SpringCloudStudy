ae6fd54a1c783889991bf891dd01e4fb45cf3e87
duanxu <duanxu@outlook.com>
2017-10-11 16:30:55 +0800

    initial

--- config-client/src/main/java/science/duanxu/configclient/service/TestController.java
+++ config-client/src/main/java/science/duanxu/configclient/service/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${from")
+    @Value("${from}")
     private String from;
 
     @Autowired
--- config-server/src/main/java/science/duanxu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/science/duanxu/configserver/ConfigServerApplication.java
@@ -1,11 +1,11 @@
 package science.duanxu.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringCloudApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
